03b0f874df2bae39302941ce1f6f7c6f63802038
Nathan Finch <nathanrfinch@outlook.com>
2016-12-21 03:54:08 +0000

    Refactor image decode into modules

---
+++ main.py
@@ -0,0 +1,126 @@
+import pprint
+from modules.image import Image
+
+def stoi(s):
+    #Sums a string of ascii chatacters as byte values
+    return sum(bytearray(s))
+
+def toHex(i):
+    #Converts an int to uppercase hex
+    return i.encode('hex').upper()
+
+def hexDec(h):
+    #Converts hex to dec
+    l = list(h)
+    s = ''
+    
+    for char in l:
+        s += toHex(char)
+        
+    return int(s, 16)
+
+def checkIdent(i):
+    #Check the magic ident at the start of a TPL file
+    data = list(i)
+
+    if toHex(data[0]) != '00':
+        return False
+    if toHex(data[1]) != '20':
+        return False
+    if toHex(data[2]) != 'AF':
+        return False
+    if toHex(data[3]) != '30':
+        return False
+
+    return True
+
+def hex2(n):
+    #Int to hex alternative
+    x = '%x' % (n,)
+    return ('0' * (len(x) % 2)) + x
+
+def iToBE(i):
+    #Int to big endian hex
+    return hex2(i).decode('hex')[::-1].encode('hex')
+
+def padToEight(s):
+    #Pad a hex string to eight characters with zeroes
+    if len(s) < 8:
+        s += ('0' * int(8 - len(s)))
+        
+    return s
+
+def padToTwo(s):
+    #Pad a hex string to two characters with zeroes
+    #TODO: generic padding function
+    if len(s) == 1:
+        s = '0' + s
+
+    return s
+
+def makeBitmapHeader(width, height, size):
+    #Create a valid header for a bitmap file using width, height, and 
+    #file size of pixels
+    width = padToEight(iToBE(width))
+    height = padToEight(iToBE(height))
+    sizeraw = padToEight(iToBE(size))
+    size = padToEight(iToBE(size + 54))
+    header = '424D' + size + '000000003600000028000000' + width + height + '0100180000000000' + sizeraw + '130B0000130B00000000000000000000'
+    return bytearray.fromhex(header)    
+
+def arrayToPixels(ar):
+    #Convert a pixel array into a linear set of pixels for bitmap
+    ret = ''
+    width = len(ar[0])
+    h = len(ar) - 1
+
+    while h >= 0:
+        w = 0
+        while w < width:
+            cpix = ar[h][w]
+            px = padToTwo(hex(cpix['b'])[2:])
+            px += padToTwo(hex(cpix['g'])[2:])
+            px += padToTwo(hex(cpix['r'])[2:])
+            ret += px
+            w += 1
+        h -= 1
+
+    print len(ret)
+    return bytearray.fromhex(ret)
+
+file = raw_input('file: ')
+f = open(file, 'rb')
+
+try:
+    ident = f.read(4)
+    if checkIdent(ident):
+        print 'TPL identifier found'
+        count = stoi(f.read(4))
+        print 'Image count: ' + str(count)
+        itoff = hexDec(f.read(4))
+        print 'Image offset table byte: ' + str(itoff)
+        f.seek(itoff)
+        i = 0
+        for i in range(0, count):
+            imoff = hexDec(f.read(4))
+            paoff = hexDec(f.read(4))
+            cuoff = f.tell()
+            f.seek(imoff)
+            imheader = f.read(35)
+            image = Image(imheader)
+            print image.format
+            #TODO: support more image formats
+            if image.format == 'CMPR':
+                f.seek(image.offset)
+                imdata = f.read(image.size)
+                image.loadImage(imdata)
+                bitmapPixels = arrayToPixels(image.getPixelArray())
+                bitmapHeader = makeBitmapHeader(image.width, image.height, image.size)
+                bm = open('C:/output/' + str(i) + '.bmp', 'wb')
+                bm.write(bitmapHeader)
+                bm.write(bitmapPixels)
+                bm.close()
+        
+            f.seek(cuoff)          
+finally:
+    f.close()
---
+++ modules/image_formats/cmpr.py
@@ -0,0 +1,100 @@
+import StringIO
+import struct
+from bitstring import BitArray
+from imageformat import ImageFormat
+
+class CMPR(ImageFormat):
+    def __init__(self, data, width, height):
+        self.width = width
+        self.height = height
+        self.loadImage(data)
+        
+    def loadImage(self, data):
+        self.createImageArray(self.width, self.height)
+        self.decodeCMPR(data)
+        
+    def decodeCMPR(self, data):
+        s = StringIO.StringIO(data)
+    
+        for j in range(0, self.height/8):
+            for i in range(0, self.width/8):
+                block = s.read(32)
+                decodedBlock = self.decodeCMPRBlock(block)
+                self.imageArray = self.listCopy2d(self.imageArray, decodedBlock, i*8, j*8)
+        
+        s.close()
+        
+    def decodeCMPRBlock(self, block):
+        bm = [[0 for i in range(8)] for j in range(8)]
+        bm = self.listCopy2d(bm, self.decodeCMPRSubblock(block[:8]), 0, 0)
+        bm = self.listCopy2d(bm, self.decodeCMPRSubblock(block[8:16]), 4, 0)
+        bm = self.listCopy2d(bm, self.decodeCMPRSubblock(block[16:24]), 0, 4)
+        bm = self.listCopy2d(bm, self.decodeCMPRSubblock(block[24:32]), 4, 4)
+        return bm
+        
+    def decodeCMPRSubblock(self, sblock):
+        pall = self.decodeCMPPalette(sblock[:4])
+        pix = BitArray(uint=struct.unpack('>I', sblock[-4:])[0], length=32)
+        j = 0
+        k = 0
+        sb = []
+    
+        for j in range(0, 4):
+            sb.append([])
+            for i in range(0, 4):
+                c = pix[k * 2:(k + 1) * 2].uint
+                if pall[c] != -1:
+                    sb[j].append(pall[c])
+                else:
+                    sb[j].append({'r': 0, 'g': 0, 'b': 0})
+                k += 1
+            
+        return sb
+        
+    def decodeCMPPalette(self, pdata):
+        #Decode the 2 byte pallette data for a CMP subblock and interpolate the additional colours
+        #TODO: check interpolation accuracy
+        cone = struct.unpack('>H', pdata[:2])[0]
+        ctwo = struct.unpack('>H', pdata[2:4])[0]
+        cthree = 0
+        cfour = 0
+    
+        if cone > ctwo:
+            cthree = self.interpolate565Colours(cone, ctwo, 1/float(3))
+            cfour = self.interpolate565Colours(cone, ctwo, 2/float(3))
+        else:
+            cthree = self.interpolate565Colours(cone, ctwo, 0.5)
+            cfour = -1
+        
+        cone = self.c565to888(cone)
+        ctwo = self.c565to888(ctwo)
+        ret = {0: cone, 1: ctwo, 2: cthree, 3: cfour}
+        return ret
+
+    def interpolate565Colours(self, one, two, amount):
+        #Interpolate between two RGB565 colours
+        one = self.c565to888(one)
+        two = self.c565to888(two)
+        return self.interpolateColours(one, two, amount)
+
+    def interpolateColours(self, one, two, amount):
+        #Interpolate between two RGB888 colours
+        cdiffr = one['r'] - two['r']
+        cdiffg = one['g'] - two['g']
+        cdiffb = one['b'] - two['b']
+        cthr = int(float(one['r']) - (cdiffr * amount))
+        cthg = int(float(one['g']) - (cdiffg * amount))
+        cthb = int(float(one['b']) - (cdiffb * amount))
+        return {'r': cthr, 'g': cthg, 'b': cthb}
+        
+    def c565to888(self, col):
+        #Convert RGB565 to RGB888
+        if col == -1:
+            return -1
+    
+        ba = BitArray(uint=int(col), length=16)
+        ret = {}
+        ret['r'] = int(round(float(ba[:5].uint) * (255 / float(32))))
+        ret['g'] = int(round(float(ba[5:11].uint) * (255 / float(64))))
+        ret['b'] = int(round(float(ba[11:16].uint) * (255 / float(32))))
+        return ret
