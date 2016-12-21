8a7f5544700943f998dcced4f984a9107310ffee
Nathan Finch <nathanrfinch@outlook.com>
2016-12-21 05:03:16 +0000

    Remove unnecessary functions

--- main.py
+++ main.py
@@ -1,61 +1,20 @@
+import struct
 from modules.image import Image
-
-def stoi(s):
-    #Sums a string of ascii chatacters as byte values
-    return sum(bytearray(s))
-
-def toHex(i):
-    #Converts an int to uppercase hex
-    return i.encode('hex').upper()
-
-def hexDec(h):
-    #Converts hex to dec
-    l = list(h)
-    s = ''
-    
-    for char in l:
-        s += toHex(char)
-        
-    return int(s, 16)
 
 def checkIdent(i):
     #Check the magic ident at the start of a TPL file
     data = list(i)
 
-    if toHex(data[0]) != '00':
+    if struct.unpack('>B', data[0])[0] != 0:
         return False
-    if toHex(data[1]) != '20':
+    if struct.unpack('>B', data[1])[0] != 32:
         return False
-    if toHex(data[2]) != 'AF':
+    if struct.unpack('>B', data[2])[0] != 175:
         return False
-    if toHex(data[3]) != '30':
+    if struct.unpack('>B', data[3])[0] != 48:
         return False
 
     return True
-
-def hex2(n):
-    #Int to hex alternative
-    x = '%x' % (n,)
-    return ('0' * (len(x) % 2)) + x
-
-def iToBE(i):
-    #Int to big endian hex
-    return hex2(i).decode('hex')[::-1].encode('hex')
-
-def padToEight(s):
-    #Pad a hex string to eight characters with zeroes
-    if len(s) < 8:
-        s += ('0' * int(8 - len(s)))
-        
-    return s
-
-def padToTwo(s):
-    #Pad a hex string to two characters with zeroes
-    #TODO: generic padding function
-    if len(s) == 1:
-        s = '0' + s
-
-    return s
 
 file = raw_input('file: ')
 f = open(file, 'rb')
@@ -64,15 +23,15 @@ try:
     ident = f.read(4)
     if checkIdent(ident):
         print 'TPL identifier found'
-        count = stoi(f.read(4))
+        count = struct.unpack('>I', f.read(4))[0]
         print 'Image count: ' + str(count)
-        itoff = hexDec(f.read(4))
+        itoff = struct.unpack('>I', f.read(4))[0]
         print 'Image offset table byte: ' + str(itoff)
         f.seek(itoff)
         
         for i in range(0, count):
-            imoff = hexDec(f.read(4))
-            paoff = hexDec(f.read(4))
+            imoff = struct.unpack('>I', f.read(4))[0]
+            paoff = struct.unpack('>I', f.read(4))[0]
             cuoff = f.tell()
             f.seek(imoff)
             imheader = f.read(35)
